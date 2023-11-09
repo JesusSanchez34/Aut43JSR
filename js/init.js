808db9efd99eb00868572ee17e8e77f8942b79fe
JesusSanchez34 <formajsr@gmail.com>
2023-11-09 10:51:40 -0600

    cambios

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyAy0Bkt5i6F8Bm6yT9YriItKBvgwY-RDAc",
+  authDomain: "auti43jsr.firebaseapp.com",
+  projectId: "auti43jsr",
+  storageBucket: "auti43jsr.appspot.com",
+  messagingSenderId: "227835384652",
+  appId: "1:227835384652:web:a80d4a50c1b4c759cff5e9",
+  measurementId: "G-YXQE5F7M5N"
+    });
